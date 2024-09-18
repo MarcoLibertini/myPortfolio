51318bedebc1c4dfb2c2eda3b1c1a0cf547b2cab
Marco Libertini <mglibertini@gmail.com>
2024-09-17 22:41:08 -0300

    Implementacion de boton alternar idiomas

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,19 +1,29 @@
+// src/components/AboutMe.js
 import React from 'react';
 
-const AboutMe = () => {
+const AboutMe = ({ language }) => {
+  const content = {
+    en: {
+      title: "Hi! I'm Marco!",
+      description: `I come from a different background than my peers, but I see it as a potential advantage.
+                    I have accumulated 15 years of experience in technical support, design, problem-solving, 
+                    and customer service. Today, I have finally become a programmer, something I've always loved since I was a child!`,
+    },
+    es: {
+      title: "¡Hola! Soy Marco!",
+      description: `Vengo de un trasfondo diferente al de mis compañeros, pero lo veo como una ventaja.
+                    He acumulado 15 años de experiencia en soporte técnico, diseño, resolución de problemas
+                    y atención al cliente. Hoy, finalmente me he convertido en programador, algo que siempre me ha encantado desde niño!`,
+    },
+  };
+
   return (
     <section className="about-me">
       <div className="about-me-content">
         <img src="/imgs/profile.png" alt="Mi Foto" className="about-me-image" />
         <div className="about-me-text">
-          <h1>Hi! I'm Marco!</h1>
-          <p>
-          I come from a different background than my peers, but I see it as a potential advantage. And I
-                        can prove it. I have accumulated 15 years of experience in technical support, design,
-                        problem-solving, and customer service. This journey has presented me with countless challenges and opportunities for
-                        development. Today, I have finally become a programmer, something I've always loved since I was
-                        a child!
-          </p>
+          <h1>{content[language].title}</h1>
+          <p>{content[language].description}</p>
         </div>
       </div>
     </section>
---
+++ src/translations/en.js
@@ -0,0 +1,72 @@
+export const projectContent = [
+    {
+      image: './imgs/bukowski.jpg',
+      title: 'Bukowski-Quote-Generator',
+      description: 'Utilizing web scraping techniques with BeautifulSoup, I compiled a list of Charles Bukowski quotes. This React application fetches a random quote from this list on each user interaction.',
+      link: 'enlace/a/tu/proyecto1',
+      codeLink: 'enlace/a/tu/codigo1'
+    },
+    {
+      image: './imgs/beerInvaders.jpg',
+      title: 'BeerInvaders GamePy',
+      description: 'A Python project using the pyGame library that is an imitation of the classic Space Invaders game, reimagined as Beer Invaders. In this game, an alien shoots beer at friends.',
+      link: '#',
+      codeLink: 'enlace/a/tu/codigo2'
+    },
+    {
+      image: './imgs/teslaSpace.jpg',
+      title: 'System Tesla/SpaceX',
+      description: 'A C# project built with Windows Forms that controls registrations, deregistrations, and modifications of Tesla and SpaceX vehicles. It also performs checks on the vehicles.',
+      link: 'enlace/a/tu/proyecto3',
+      codeLink: 'enlace/a/tu/codigo3'
+    },
+    {
+      image: './imgs/dangerzone.jpg',
+      title: 'Danger Zone',
+      description: 'This project utilizes a dataset of crimes in the United States to create a Kmeans model. The model categorizes areas into low, medium, and high danger levels and maps them with characteristic colors.',
+      link: 'enlace/a/tu/proyecto1',
+      codeLink: 'enlace/a/tu/codigo4'
+    },
+    {
+      image: './imgs/beerSelector.jpg',
+      title: 'Beer Recommendation',
+      description: 'Using data analysis on a dataset of beer-related information, this project creates a machine learning model. The model recommends beers based on user preferences, with an interactive method for modifying preferences.',
+      link: 'enlace/a/tu/proyecto2',
+      codeLink: 'enlace/a/tu/codigo5'
+    },
+    {
+      image: './imgs/dashboard.jpg',
+      title: 'Dashboard',
+      description: 'Full Stack Final Project: A Single Page Application (SPA) developed as the final project for a Full Stack course. This project involves creating a comprehensive user management system, including user, client, stock, purchase, sale, product, and service management.',
+      link: 'enlace/a/tu/proyecto4',
+      codeLink: 'enlace/a/tu/codigo6'
+    },
+    {
+      image: './imgs/mush.png',
+      title: 'MagicFun',
+      description: 'A basic project for managing types of mushrooms using Flask, SQL, and JavaScript would involve creating a CRUD (Create, Read, Update, Delete) application.',
+      link: 'enlace/a/tu/proyecto1',
+      codeLink: 'enlace/a/tu/codigo7'
+    },
+    {
+      image: './imgs/storee.png',
+      title: 'Spotify Classifier',
+      description: 'Spotify theme classifier, evaluating whether a user might like a track based on its features, using data analysis and a machine learning model.',
+      link: 'enlace/a/tu/proyecto2',
+      codeLink: 'enlace/a/tu/codigo8'
+    },
+    {
+      image: './imgs/astro.jpg',
+      title: 'Story Collector',
+      description: 'A story collector, gathering recounted stories, amusing or not so amusing anecdotes, but ones that are remembered.',
+      link: 'enlace/a/tu/proyecto4',
+      codeLink: 'enlace/a/tu/codigo9'
+    },
+    {
+      image: './imgs/aho.png',
+      title: 'Hangman GamePy',
+      description: 'A Python project that imitates the famous Hangman game, where the user guesses a hidden word chosen by the computer by selecting letters. The user has a limited number of attempts, and if they don\'t guess the word within the correct number of attempts, they lose.',
+      link: 'enlace/a/tu/proyecto1',
+      codeLink: 'enlace/a/tu/codigo1'
+    }
+  ]
